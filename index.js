d4569d1cf3a95e750aea14ea1990a4e5818a22e9
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-03 11:43:06 -0700

    Added comments, whitespace checker for search.

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+// Find Search Term function
+//
+// The function accepts a search term and an array of books.
+// It prepares a results object to return and runs a nested loop to iterate through each book and its
+// content. If there is a match in the search, a result object is created with ISBN, Page, and Line and
+// pushed to the results object. Once the iteration is complete the results object is returned and
+// logged to the console.
+function findSearchTermInBooks(searchTerm, books) {
+  const resultsObject = {
+    SearchTerm: searchTerm,
+    Results: [],
+  };
+
+  for (const book of books) {
+    for (const page of book.Content) {
+      if (page.Text.includes(searchTerm)) {
+        const result = {
+          ISBN: book.ISBN,
+          Page: page.Page,
+          Line: page.Line,
+        };
+        resultsObject.Results.push(result);
+      }
+    }
+  }
+
+  console.log(resultsObject);
+
+  return resultsObject;
+}
+
+// DOM Manipulators
 const searchButton = document.getElementById("search-button");
 const searchText = document.getElementById("search-text");
 const resultsBody = document.getElementById("results-body");
 
+// Event listener for search button.
+// Although it wasn't necessary, I decided to make this a full HTML/CSS/JS app with UI.
+// If the search bar is empty, it will alert the user to add in a search parameter and return.
+// It will also return if the user has only put in whitespace.
+// With a search term added, the findSearchTermInBooks runs and logs the results to the console.
+// It also populates the results in the resultsBody element in the HTML document.
 searchButton.addEventListener("click", () => {
   if (searchText.value === "") {
     alert("Search bar cannot be empty.");
+    return;
+  } else if (searchText.value.trim() === "") {
+    alert("Search bar can't contain only spaces.");
     return;
   }
 
@@ -21,6 +62,7 @@ searchButton.addEventListener("click", () => {
   }
 });
 
+// Array for the books
 const booksArr = [
   {
     Title: "Shakespeare's Sonnet #115",
@@ -66,29 +108,17 @@ const booksArr = [
   },
 ];
 
-function findSearchTermInBooks(searchTerm, books) {
-  const resultsObject = {
-    SearchTerm: searchTerm,
-    Results: [],
-  };
+// Unit Tests
 
-  const search = searchTerm.toUpperCase();
+// Case-Sensitive Unit Tests
+findSearchTermInBooks("the", booksArr);
+findSearchTermInBooks("The", booksArr);
 
-  for (const book of books) {
-    for (const page of book.Content) {
-      const pageText = page.Text.toUpperCase();
-      if (pageText.includes(search)) {
-        const result = {
-          ISBN: book.ISBN,
-          Page: page.Page,
-          Line: page.Line,
-        };
-        resultsObject.Results.push(result);
-      }
-    }
-  }
+// Hits/Positive tests:
+findSearchTermInBooks("myself", booksArr);
 
-  console.log(resultsObject);
+// Misses/Negative Tests:
+findSearchTermInBooks("Zootopia", booksArr);
 
-  return resultsObject;
-}
+// Full Phrase Test:
+findSearchTermInBooks("I asked myself", booksArr);

903ff130cd41dbe155a78f1aedd575f42044b368
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-03 10:18:25 -0700

    Added results body in HTML. Added event listener.

---
+++ index.js
@@ -0,0 +1,8 @@
+const searchButton = document.getElementById("search-button");
+const searchtext = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+  console.log(searchtext.value);
+});
+
+function findSearchTermInBooks(searchTerm, books) {}
